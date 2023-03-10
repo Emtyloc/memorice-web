e60b1a648a1e7b0d142530bac1b55baeb222dc69
Emtyloc <mitoro1@miuandes.cl>
2023-03-10 20:45:56 -0300

    btns underline

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const timesBtn = document.getElementById("tiempos-btn");
 triesBtn.onclick = () => {
   document.getElementById("score-tables-tries").style.display = "flex";
   document.getElementById("score-tables-time").style.display = "none";
+  triesBtn.style.textDecoration = "underline";
+  timesBtn.style.textDecoration = "none";
 };
 
 timesBtn.onclick = () => {
   document.getElementById("score-tables-time").style.display = "flex";
   document.getElementById("score-tables-tries").style.display = "none";
+  timesBtn.style.textDecoration = "underline";
+  triesBtn.style.textDecoration = "none";
 };
