20adf84c877726309e19ad92bdc3c304ef7da4a0
Emtyloc <mitoro1@miuandes.cl>
2023-03-10 21:11:33 -0300

    remove style from js

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ const timesBtn = document.getElementById("tiempos-btn");
 triesBtn.onclick = () => {
   document.getElementById("score-tables-tries").style.display = "flex";
   document.getElementById("score-tables-time").style.display = "none";
-  triesBtn.style.textDecoration = "underline";
-  timesBtn.style.textDecoration = "none";
+  if (timesBtn.classList.contains("selected-table-btn")) {
+    timesBtn.classList.remove("selected-table-btn");
+  }
+  if (!triesBtn.classList.contains("selected-table-btn"))
+    triesBtn.classList.add("selected-table-btn");
 };
 
 timesBtn.onclick = () => {
   document.getElementById("score-tables-time").style.display = "flex";
   document.getElementById("score-tables-tries").style.display = "none";
-  timesBtn.style.textDecoration = "underline";
-  triesBtn.style.textDecoration = "none";
+  if (triesBtn.classList.contains("selected-table-btn")) {
+    triesBtn.classList.remove("selected-table-btn");
+  }
+  if (!timesBtn.classList.contains("selected-table-btn"))
+    timesBtn.classList.add("selected-table-btn");
 };
