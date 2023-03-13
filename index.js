0560534edb2693ebbdae7b86e7cb9067d0a50693
Emtyloc <mitoro1@miuandes.cl>
2023-03-12 23:53:55 -0300

    remove .style from js

--- index.js
+++ index.js
@@ -2,21 +2,31 @@ const triesBtn = document.getElementById("intentos-btn");
 const timesBtn = document.getElementById("tiempos-btn");
 
 triesBtn.onclick = () => {
-  document.getElementById("score-tables-tries").style.display = "flex";
-  document.getElementById("score-tables-time").style.display = "none";
-  if (timesBtn.classList.contains("selected-table-btn")) {
-    timesBtn.classList.remove("selected-table-btn");
-  }
-  if (!triesBtn.classList.contains("selected-table-btn"))
-    triesBtn.classList.add("selected-table-btn");
+  document.getElementById("score-tables-tries").classList.add("showing-table");
+  document
+    .getElementById("score-tables-tries")
+    .classList.remove("hidding-table");
+
+  document.getElementById("score-tables-time").classList.add("hidding-table");
+  document
+    .getElementById("score-tables-time")
+    .classList.remove("showing-table");
+
+  timesBtn.classList.remove("selected-table-btn");
+  triesBtn.classList.add("selected-table-btn");
 };
 
 timesBtn.onclick = () => {
-  document.getElementById("score-tables-time").style.display = "flex";
-  document.getElementById("score-tables-tries").style.display = "none";
-  if (triesBtn.classList.contains("selected-table-btn")) {
-    triesBtn.classList.remove("selected-table-btn");
-  }
-  if (!timesBtn.classList.contains("selected-table-btn"))
-    timesBtn.classList.add("selected-table-btn");
+  document.getElementById("score-tables-time").classList.add("showing-table");
+  document
+    .getElementById("score-tables-time")
+    .classList.remove("hidding-table");
+
+  document.getElementById("score-tables-tries").classList.add("hidding-table");
+  document
+    .getElementById("score-tables-tries")
+    .classList.remove("showing-table");
+
+  triesBtn.classList.remove("selected-table-btn");
+  timesBtn.classList.add("selected-table-btn");
 };
