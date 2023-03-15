f7380f942efef3bf27f75c8cd4cc77d32a48cedb
Emtyloc <mitoro1@miuandes.cl>
2023-03-14 21:32:04 -0300

    side bar

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const triesBtn = document.getElementById("intentos-btn");
 const timesBtn = document.getElementById("tiempos-btn");
 const navBar = document.getElementById("top-nav-div");
 const settingsBtn = document.getElementById("settings-btn");
+const sideBar = document.getElementById("side-bar");
+const cardsTable = document.getElementsByClassName("cards-table")[0];
 
 triesBtn.onclick = () => {
   document.getElementById("score-tables-tries").classList.add("showing-table");
@@ -41,3 +43,30 @@ navBar.onclick = () => {
     ? (settingsBtn.innerText = "cerrar")
     : (settingsBtn.innerText = "configuracion");
 };
+
+// appear or desappear side bar
+window.addEventListener("scroll", () => {
+  if (window.matchMedia("(orientation: landscape)").matches) {
+    if (document.documentElement.scrollTop >= 85) {
+      sideBar.classList.add("hidding-sidebar");
+      cardsTable.classList.add("cards-table-no-margin");
+    }
+
+    if (document.documentElement.scrollTop <= 15) {
+      sideBar.classList.remove("hidding-sidebar");
+      cardsTable.classList.remove("cards-table-no-margin");
+    }
+  }
+});
+let portrait = window.matchMedia("(orientation: portrait)");
+portrait.addEventListener("change", function (e) {
+  if (e.matches) {
+    sideBar.classList.add("hidding-sidebar");
+    cardsTable.classList.add("cards-table-no-margin");
+  } else {
+    if (document.documentElement.scrollTop <= 15) {
+      sideBar.classList.remove("hidding-sidebar");
+      cardsTable.classList.remove("cards-table-no-margin");
+    }
+  }
+});
