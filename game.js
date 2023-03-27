d7155cf040e785075b61693fed1042a4b1c66289
Emtyloc <mitoro1@miuandes.cl>
2023-03-26 23:29:54 -0300

    bug fix

--- game.js
+++ game.js
@@ -7,6 +7,8 @@ let firstCard, secondCard;
 //function to flip card and detect if is first or second card
 function flipCard() {
   if (lockTable) return;
+  if (this === firstCard) return;
+
   this.classList.add("flip");
 
   //if flippedCard == false then is the first card
@@ -17,7 +19,6 @@ function flipCard() {
   }
   //else is the second card and we check for a match
   secondCard = this;
-  flippedCard = false;
 
   checkForMatch();
 }
@@ -33,6 +34,8 @@ function checkForMatch() {
 function disableCards() {
   firstCard.removeEventListener("click", flipCard);
   secondCard.removeEventListener("click", flipCard);
+
+  resetTable();
 }
 
 //flip cards back after 1.2 seconds.
@@ -41,8 +44,16 @@ function unflipCards() {
   setTimeout(() => {
     firstCard.classList.remove("flip");
     secondCard.classList.remove("flip");
-    lockTable = false;
+
+    resetTable();
   }, 1200);
+}
+
+//set default values for flippedCard and lockTable,
+// and reset first and second Cards values to null
+function resetTable() {
+  [flippedCard, lockTable] = [false, false];
+  [firstCard, secondCard] = [null, null];
 }
 
 //adding click event listener for all cards, and calls flipCard().
