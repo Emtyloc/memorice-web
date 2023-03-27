93c4e076beb0a799bc4fe94fcf70435591ff4ae0
Emtyloc <mitoro1@miuandes.cl>
2023-03-26 21:26:24 -0300

    matching integration

--- game.js
+++ game.js
@@ -3,31 +3,37 @@ const cards = document.querySelectorAll(".card");
 let flippedCard = false;
 let firstCard, secondCard;
 
+//function to flip card and detect if is first or second card
 function flipCard() {
   this.classList.add("flip");
 
+  //if flippedCard == false then is the first card
   if (!flippedCard) {
     flippedCard = true;
     firstCard = this;
     return;
   }
-
+  //else is the second card and we check for a match
   secondCard = this;
   flippedCard = false;
 
   checkForMatch();
 }
 
+//check if the cards are pairs, if they are then disabled them,
+// else flip them back.
 function checkForMatch() {
   let isMatch = firstCard.dataset.icon === secondCard.dataset.icon;
   isMatch ? disableCards() : unflipCards();
 }
 
+//remove click actions for paired cards.
 function disableCards() {
   firstCard.removeEventListener("click", flipCard);
   secondCard.removeEventListener("click", flipCard);
 }
 
+//flip cards back after 1.2 seconds.
 function unflipCards() {
   setTimeout(() => {
     firstCard.classList.remove("flip");
@@ -35,4 +41,5 @@ function unflipCards() {
   }, 1200);
 }
 
+//adding click event listener for all cards, and calls flipCard().
 cards.forEach((card) => card.addEventListener("click", flipCard));
